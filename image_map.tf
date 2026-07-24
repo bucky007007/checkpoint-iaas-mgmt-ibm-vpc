4d0ff01e44e9dcdc6d0b90daae6887ca5d1f0fcb
bucky007007 <65685954+bucky007007@users.noreply.github.com>
2026-07-24 10:52:46 -0500

    Update image_map.tf

--- image_map.tf
+++ image_map.tf
@@ -1,6 +1,22 @@
 locals { 
   image_map = {
-   R8120-Management = {
+    R82-Management = {
+      "us-south" = ""
+      "us-east"  = ""
+      "eu-gb"    = ""
+      "eu-de"    = ""
+      "eu-es"    = ""
+      "jp-tok"   = ""
+      "jp-osa"   = ""
+      "au-syd"   = ""
+      "ca-mon"   = ""
+      "ca-tor"   = ""
+      "in-che"   = ""
+      "in-mum"   = ""
+      "br-sao"   = ""
+    }
+
+    R8120-Management = {
       "us-south" = "r006-ba12a2e6-f897-4db0-a379-d61af05f2e5f"
       "us-east"  = "r014-897d2ef8-6e02-4d55-b3f1-f400d7aa9514"
       "eu-gb"    = "r018-329e0e6f-4f47-4ca5-829e-74367256e381"
@@ -13,20 +29,6 @@ locals {
       "br-sao"   = "r042-2967c5aa-7622-4757-862e-426524fef90f"
     }
 
-    R8120-Gateway = {
-      "us-south" = "r006-33f35d30-b5a9-4d13-b770-39ef2fc1c665"
-      "us-east"  = "r014-1cac7a47-a902-4b12-bc4b-2ae8e2af1673"
-      "eu-gb"    = "r018-769376eb-6bdf-4e3e-b6f5-11eb4ea54941"
-      "eu-de"    = "r010-43730e24-58f9-4878-b350-136c3e298985"
-      "eu-es"    = "r050-f893bf42-e9c6-4b3c-8007-e5700925fd96"
-      "jp-tok"   = "r022-6892d611-ab7a-4954-af04-5f899c0138de"
-      "jp-osa"   = "r034-356f570b-b5f2-4e2d-b206-8a57835ac038"
-      "au-syd"   = "r026-2a7047ea-8885-45af-a6ed-816a933ca842"
-      "ca-tor"   = "r038-46e80e0c-4941-4a73-ade6-574272cf9fd5"
-      "br-sao"   = "r042-50c06aa2-af36-4535-a91b-4082512ac9dd"
-	  
- 
-    }
     R8110-Management = {
       "us-south" = "r006-79cb86a8-a0b0-40df-92dd-6ae750caa765"
       "us-east"  = "r014-61feb151-2ef1-4df4-bd51-4cd02d2de253"
